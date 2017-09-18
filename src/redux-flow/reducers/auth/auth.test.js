8c6a438c777f93d3ca71984a4bd98b9a4ecbda03
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2017-09-18 17:33:04 -0300

    ajustando testes de reducers

--- src/redux-flow/reducers/auth/auth.test.js
+++ src/redux-flow/reducers/auth/auth.test.js
@@ -4,20 +4,24 @@ import { expect } from 'chai'
 import reducer, { initialState } from './index'
 import * as action from './actions'
 
-describe('participants reducer', () => {
+describe('auth reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).to.be.deep.equal(initialState)
   })
 
-  it('should handle FETCHING', () => {
-    expect(reducer([], { type: action.FETCHING })).to.be.deep.equal({ isFetching: true })
-  })
+  it('should handle LOGIN_REQUEST', () => {
+    const dispatcher = {
+      type: action.LOGIN_REQUEST,
+      payload: {
+        email: 'admin@admin.com.br',
+        password: 'password'
+      }
+    }
 
-  it('should handle SUCCESS', () => {
-    expect(reducer([], { type: action.SUCCESS, payload: {} })).to.be.deep.equal({ list: {}, isFetching: false })
-  })
-
-  it('should handle ERROR', () => {
-    expect(reducer([], { type: action.ERROR })).to.be.deep.equal({ isFetching: false })
+    expect(reducer([], dispatcher)).to.be.deep.equal({
+      isFetching: true,
+      isAuthenticated: false,
+      user: dispatcher
+    })
   })
 })
