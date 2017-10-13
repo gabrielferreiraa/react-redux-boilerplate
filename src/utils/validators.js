c734f6194ca7e4929d2006f56d33de41e7bf7b89
Gabriel Ferreira <gabriel.alves@itlean.com.br>
2017-10-12 23:04:49 -0300

    fix validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,3 +3,4 @@
 export const required = value => (value ? undefined : 'O campo :name: é obrigatório')
 export const number = value => value && isNaN(Number(value)) ? 'O campo :name: precisa ser numérico' : undefined
 export const minValue = min => value => value && value < min ? `:name: deve ser maior que ${min}` : undefined
+export const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'O campo :name: é inválido' : undefined
